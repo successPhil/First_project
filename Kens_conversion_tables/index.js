67accd8e089c1476cf443e7a711c031c13bd1604
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-04-26 19:03:51 -0400

    First version

---
+++ Kens_conversion_tables/index.js
@@ -0,0 +1,6 @@
+function getResponse() {
+    let input = document.getElementById("input").value;
+    let output = document.getElementById("output");
+    output.innerHTML = "You typed: " + input;
+  }
+  
